b2a491a1782361466ac63bd47dbf40eb042d1d03
takebou <tk021125@outlook.jp>
2022-02-06 20:56:28 +0900

    initial commit

---
+++ abs/abc085c/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	if len(os.Args) >= 2 {
+		if os.Args[1] == "debug" {
+			debug()
+		}
+	}
+	sc.Split(bufio.ScanWords)
+}
+
+func debug() {
+	testFile, err := os.Open("./tests/sample-1.in")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "There is no testfile.")
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(testFile)
+}
+
+func readS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readI() int {
+	i, _ := strconv.Atoi(readS())
+	return i
+}
+
+func readR() []rune {
+	return []rune(readS())
+}
+
+func digit(i int, list []int) []int {
+	if i > 0 {
+		return digit(i/10, append(list, i%10))
+	}
+	return list
+}
+
+func main() {
+	N := readI()
+	Y := readI()
+
+	for i := 0; i <= N; i++ {
+		for k := 0; k <= N-i; k++ {
+			j := N - (i + k)
+			if i*10000+k*5000+j*1000 == Y {
+				fmt.Println(i, k, j)
+				os.Exit(0)
+			}
+
+		}
+	}
+
+	fmt.Println("-1 -1 -1")
+
+}
