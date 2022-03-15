ed31e9d1b3b2bf5f50beff47a006602b797e006b
takebou <tk021125@outlook.jp>
2022-03-15 21:56:28 +0900

    reverse追加

--- template.go
+++ template.go
@@ -41,6 +41,15 @@ func readR() []rune {
 	return []rune(readS())
 }
 
+func reverseS(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	return string(r)
+}
+
 func digit(i int, list []int) []int {
 	if i > 0 {
 		return digit(i/10, append(list, i%10))
