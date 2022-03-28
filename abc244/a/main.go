2ea18e009c3ea677e4c055c8c141bdacda6398b7
takebou <tk021125@outlook.jp>
2022-03-29 00:14:18 +0900

    initial commit

---
+++ abc244/a/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	if len(os.Args) >= 2 {
+		if os.Args[1] == "debug" {
+			debug()
+		}
+	}
+	sc.Split(bufio.ScanWords)
+}
+
+func debug() {
+	testFile, err := os.Open("./tests/sample-1.in")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "There is no testfile.")
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(testFile)
+}
+
+func readS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readI() int {
+	i, _ := strconv.Atoi(readS())
+	return i
+}
+
+func readR() []rune {
+	return []rune(readS())
+}
+
+func reverseS(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	return string(r)
+}
+
+func digit(i int, list []int) []int {
+	if i > 0 {
+		return digit(i/10, append(list, i%10))
+	}
+	return list
+}
+
+func main() {
+	sc.Buffer(make([]byte, 64*1024), 100001)
+	N := readI()
+	S := readS()
+
+	fmt.Println(S[N-1:])
+
+}
