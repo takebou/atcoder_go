6892cd7c71723ca82a193f1f6c79141e75f210ca
takebou <tk021125@outlook.jp>
2022-06-06 22:28:05 +0900

    initial commit

---
+++ abc244/c/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	if len(os.Args) >= 2 {
+		if os.Args[1] != "debug" {
+			debug()
+		}
+	}
+	sc.Split(bufio.ScanWords)
+}
+
+func debug() {
+	testFile, err := os.Open("./tests/sample-1.in")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "There is no testfile.")
+		os.Exit(1)
+	}
+	sc = bufio.NewScanner(testFile)
+}
+
+func readS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func readI() int {
+	i, _ := strconv.Atoi(readS())
+	return i
+}
+
+func readR() []rune {
+	return []rune(readS())
+}
+
+func reverseS(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	return string(r)
+}
+
+func digit(i int, list []int) []int {
+	if i > 0 {
+		return digit(i/10, append(list, i%10))
+	}
+	return list
+}
+
+func main() {
+	sc.Buffer(make([]byte, 64*1024), 100001)
+	N := readI()
+	used := make([]bool, N*2+1)
+
+	for {
+		for i, v := range used {
+			if v == false {
+				T := i
+				used[T] = true
+				fmt.Println(T + 1)
+				break
+			}
+		}
+		A := readI()
+		if A == 0 {
+			os.Exit(0)
+		}
+		used[A-1] = true
+	}
+}
